42912e01c1b06140fa856ccad113d9ee3be23429
jck-bit <jckkinyanjui@gmail.com>
2025-02-22 18:31:07 +0300

    removed the console.log

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,7 @@ const App = () => {
         }
 
         const data = await response.json();
-        console.log(data);
+        //console.log(data);
 
         if (data && data.images) {
          
