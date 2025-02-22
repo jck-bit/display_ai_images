70bccc2673ffdf431f96afea66eb2a3f6c9bd222
jck-bit <jckkinyanjui@gmail.com>
2025-02-22 18:55:26 +0300

    resolved tailwind issues

--- vite.config.ts
+++ vite.config.ts
@@ -5,16 +5,4 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  
-  //ignore vite build error
-  server: {
-    //build command will not fail if there are errors
-    //in the code
-    //if set to true, the build will fail if there are errors
-    //in the code
-    hmr: {
-      overlay: false
-    }
-}
-
 })
