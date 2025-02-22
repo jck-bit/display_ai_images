38d49017913cf1c64821058a0a1fb8a78a12a004
jck-bit <jckkinyanjui@gmail.com>
2025-02-22 18:42:55 +0300

    removed build command

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,16 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  
+  //ignore vite build error
+  server: {
+    //build command will not fail if there are errors
+    //in the code
+    //if set to true, the build will fail if there are errors
+    //in the code
+    hmr: {
+      overlay: false
+    }
+}
+
 })
